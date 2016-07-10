b667350fd882ff5d01cfde009f2abee88003b3a8
Oleg Makiienko <oleg.makiienko@prosiebensat1.com>
2016-07-10 21:25:49 +0200

    Static render

--- index.js
+++ index.js
@@ -4,14 +4,46 @@ const glob = require('glob');
 const YAML = require('yamljs');
 const fs = require('fs');
 
+/*
+    center:
+    - -2.3556644404880812e+16
+    - 1.5954181047741544e+16
+    - -1.3573020734138632e+17
+
+    security: -0.01431927738433703
+    solarSystemID: 30001047
+    // solarSystemNameID: 270011
+    stargates:
+        50003703:
+            destination: 50002839
+            position:
+            - 211013591040.0
+            - 10564853760.0
+            - 222924103680.0
+            typeID: 29633
+
+        50003704:
+            destination: 50003220
+            position:
+            - 211003269120.0
+            - 10597294080.0
+            - 222930984960.0
+            typeID: 29633
+        
+        50013968:
+            destination: 50013967
+            position:
+            - -2210588467200.0
+            - 1364110172160.0
+            - 1741606871040.0
+            typeID: 29634
+ */
 
 let filename = process.argv[2];
 
 glob(`${__dirname}/data/sde/fsd/universe/${filename}/**/solarsystem.staticdata`, processFiles);
 
 function processFiles(e, fileNames) {
-    
-    // fileNames = fileNames.slice(0, 50);
 
     let count = fileNames.length;
     let gateToSystemMap = {}; // gate -> system
@@ -44,7 +76,6 @@ function processFiles(e, fileNames) {
             resultingObject.stargates.push(nativeObject.stargates[key].destination);
         }
         systems[name] = resultingObject;
-        // console.log(resultingObject);
     }
     // transform gates data
     let keys = Object.keys(systems);
@@ -60,10 +91,6 @@ function processFiles(e, fileNames) {
         }
         systems[key].stargates = list;
     }
-
-    // console.log(systems);
-    // console.log(gateToSystemMap);
-
     fs.writeFile(`./data/${filename}.js`, `var json${filename} = ${JSON.stringify(systems, null, '\t')}`, function(err) {
         if(err) {
             return console.log(err);
@@ -71,39 +98,3 @@ function processFiles(e, fileNames) {
         console.log(`The ${filename} was saved!`);
     }); 
 }
-/*
-center:
-- -2.3556644404880812e+16
-- 1.5954181047741544e+16
-- -1.3573020734138632e+17
-
-security: -0.01431927738433703
-solarSystemID: 30001047
-// solarSystemNameID: 270011
-stargates:
-    50003703:
-        destination: 50002839
-        position:
-        - 211013591040.0
-        - 10564853760.0
-        - 222924103680.0
-        typeID: 29633
-
-    50003704:
-        destination: 50003220
-        position:
-        - 211003269120.0
-        - 10597294080.0
-        - 222930984960.0
-        typeID: 29633
-    
-    50013968:
-        destination: 50013967
-        position:
-        - -2210588467200.0
-        - 1364110172160.0
-        - 1741606871040.0
-        typeID: 29634
-
-
- */
